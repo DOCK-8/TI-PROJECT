ceb72b2dd88e2007268cb3f6309d77f37ccec5b4
MaxsForocca <mforocca@unsa.edu.pe>
2025-06-29 00:51:05 -0500

    logic/ inicial

---
+++ logic/calcular_roi.py
@@ -0,0 +1,6 @@
+def calcularROI(costo_total, ahorros_ani):
+    if costo_total <= 0:
+        raise ValueError("El costo total debe ser mayor que cero.")
+    
+    roi = (costo_total / ahorros_ani)
+    return roi
