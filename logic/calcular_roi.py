fc52ef685c1230653cad7e9c6ce36cdd7c1af969
DOCK <franwillaa@gmail.com>
2025-07-06 12:44:12 -0500

    clear folder

--- logic/calcular_roi.py
+++
@@ -1,8 +0,0 @@
-
-
-def calcularROI(costo_total, ahorros_ani):
-    if costo_total <= 0:
-        raise ValueError("El costo total debe ser mayor que cero.")
-    
-    roi = (costo_total / ahorros_ani)
-    return roi
