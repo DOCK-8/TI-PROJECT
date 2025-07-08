1bc12e378f3f531fb04fab54d6aabc35cd900a27
GabSoto <gsotocco@unsa.edu.pe>
2025-07-07 20:29:45 -0500

    Agregabdi try catch al método de la API.

--- BACKEND/app/PVAPIRest/api/views.py
+++ BACKEND/app/PVAPIRest/api/views.py
@@ -8,6 +8,7 @@ from logic.ModelAgent import ModelAgent
 from logic.buscar_sistema_optimo import buscar_configuracion_optima
 from logic.calcular_roi import calcularResumenROIyPayback
 from django.conf import settings
+from rest_framework import status
 import json
 import os
 
@@ -55,23 +56,38 @@ def get_token_api(request):
 
 @api_view(['POST'])
 def solucionOptima(request):
-    consumo = request.data.get('consumo_total','')
-    area = request.data.get('area_disponible','')
-    modelT = TrainerModel(float(area), float(consumo), 0.1898, 0.9405, 0.8931, 0.98)
-    modelT.entrenar_modelo_completo()
-    LLP_U = 0.01
-    modelo = ModelAgent()
-    c1 = modelT.c1
-    c2 = modelT.c2
-    c3 = modelT.c3
-    c4 = modelT.c4
-    c5 = modelT.c5
-    c6 = modelT.c6
-    oPP, oCB = modelo.generateOptimoConfig(c1,c2,c3,c4,c5,c6,LLP_U)
-    configuracion = buscar_configuracion_optima(oPP,area,oCB)
-    costo_total = configuracion['panel_optimo']['costo_total_usd']*configuracion['panel_optimo']['cantidad']+\
-        configuracion['inversor_optimo']['costo_total']*configuracion['inversor_optimo']['cantidad']+\
-        configuracion['bateria_optima']['costo_total_usd']*configuracion['bateria_optima']['cantidad']
-    answer = calcularResumenROIyPayback(costo_total, oPP, float(consumo), 0.70305)
+    try:
+        consumo = request.data.get('consumo_total', '')
+        area = request.data.get('area_disponible', '')
+        
+        modelT = TrainerModel(float(area), float(consumo), 0.1898, 0.9405, 0.8931, 0.98)
+        modelT.entrenar_modelo_completo()
+
+        LLP_U = 0.01
+        modelo = ModelAgent()
+
+        c1 = modelT.c1
+        c2 = modelT.c2
+        c3 = modelT.c3
+        c4 = modelT.c4
+        c5 = modelT.c5
+        c6 = modelT.c6
+
+        oPP, oCB = modelo.generateOptimoConfig(c1, c2, c3, c4, c5, c6, LLP_U)
+        configuracion = buscar_configuracion_optima(oPP, area, oCB)
+
+        costo_total = (
+            configuracion['panel_optimo']['costo_total_usd'] * configuracion['panel_optimo']['cantidad'] +
+            configuracion['inversor_optimo']['costo_total'] * configuracion['inversor_optimo']['cantidad'] +
+            configuracion['bateria_optima']['costo_total_usd'] * configuracion['bateria_optima']['cantidad']
+        )
+
+        answer = calcularResumenROIyPayback(costo_total, oPP, float(consumo), 0.70305)
+
+        return Response({"configuracion": configuracion, "roi": answer})
     
-    return Response({"configuracion":configuracion,"roi":answer})
+    except Exception as e:
+        return Response(
+            {"error": "Error en los datos proporcionados. Revisa los valores ingresados."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
